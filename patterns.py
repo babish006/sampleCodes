ae11b712f833eeebb40d5efcc4119af2b14ff81d
Babeesh Cherukutty <bab>
2020-04-24 04:22:45 +0530

    Added extra code.

--- patterns.py
+++ patterns.py
@@ -16,3 +16,19 @@ for j in range(4):
     for i in range(4-j):
         print('# ', end='')
     print('')
+
+
+# The staircase function.
+def staircase(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(' ',end='')
+
+        for k in range(i+1):
+            print('#',end='')
+        print()
+
+if __name__ == '__main__':
+    n = int(input('Enter the size of staircase: '))
+
+    staircase(n)
